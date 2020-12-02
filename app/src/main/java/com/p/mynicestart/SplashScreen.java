f99ec8772ca26c7566071291cfa0cf900a5067e3
pablodiazsanz <Githubpablo2019>
2020-12-02 16:59:39 +0100

    readme

--- app/src/main/java/com/p/mynicestart/SplashScreen.java
+++ app/src/main/java/com/p/mynicestart/SplashScreen.java
@@ -2,11 +2,17 @@ package com.p.mynicestart;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Typeface;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions;
 
 
 public class SplashScreen extends Activity {
@@ -14,9 +20,21 @@ public class SplashScreen extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash_screen);
 
-        TextView mySubtitle = (TextView) findViewById(R.id.txt_Subtitle);
+        ImageView imageView = (ImageView) findViewById(R.id.imageView4) ;
+      //  TextView mySubtitle = (TextView) findViewById(R.id.txt_Subtitle);
+      //  Typeface face = Typeface.createFromAsset(getAssets(),
+      //          "fonts/epimodem.ttf");
+      //  mySubtitle.setTypeface(face);
         Animation myanim = AnimationUtils.loadAnimation(this, R.anim.alpha);
-        mySubtitle.startAnimation(myanim);
+        imageView.startAnimation(myanim);
+
+        ImageView fondo = findViewById(R.id.imageView3);
+        Glide.with(this)
+                .load(R.drawable.tenis_fondo)
+                .centerCrop()
+                .transition(DrawableTransitionOptions.withCrossFade(500))
+                .placeholder(new ColorDrawable(this.getResources().getColor(R.color.light_purple_button)))
+                .into(fondo);
 
         openApp(true);
     }
